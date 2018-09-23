c9e1375c917fff18baac9733af6c470dd6c8b016
Gabriel Tan <tangabrielkl@gmail.com>
2018-09-23 19:12:53 -0400

    Added GUI controls

--- HangulCharToSound/Form1.Designer.cs
+++ HangulCharToSound/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HangulCharToSound
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBoxStrInput = new System.Windows.Forms.RichTextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxStrInput
+            // 
+            this.richTextBoxStrInput.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxStrInput.Name = "richTextBoxStrInput";
+            this.richTextBoxStrInput.Size = new System.Drawing.Size(445, 426);
+            this.richTextBoxStrInput.TabIndex = 0;
+            this.richTextBoxStrInput.Text = "";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(464, 13);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(172, 110);
+            this.buttonConvert.TabIndex = 1;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.richTextBoxStrInput);
+            this.Name = "MainForm";
+            this.Text = "Hangul To Sound Character Converter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxStrInput;
+        private System.Windows.Forms.Button buttonConvert;
     }
 }
 
